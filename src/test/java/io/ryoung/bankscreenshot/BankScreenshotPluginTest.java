7a560f0b7d9f0c82b2ca0c61fefaba20f00b3dc3
Ron Young <admin@ryoung.io>
2020-06-05 23:03:34 -0500

    screenshot bank

--- src/test/java/io/ryoung/bankscreenshot/BankScreenshotPluginTest.java
+++ src/test/java/io/ryoung/bankscreenshot/BankScreenshotPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package io.ryoung.bankscreenshot;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class BankScreenshotPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(BankScreenshotPlugin.class);
 		RuneLite.main(args);
 	}
 }
